23048e1dd920e59564098760ff8bda70d91c0a63
SumaiyaAK <sumaiyasrn6@gmail.com>
2023-07-24 18:52:17 +0600

    All colleges page complete

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000
+
+const AllCollege = require('./ApiData/AllCollege.json')
+
+app.use(cors());
 
 app.get('/', (req, res)=> {
     res.send('EduConnect is running')
+})
+
+app.get('/AllCollege', (req, res) =>{
+    res.send(AllCollege)
 })
 
 app.listen(port, () => {
